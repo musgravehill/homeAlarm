67fa4ce981c4df26414197114e6f2f1733d1c543
Boris <mdeed87@gmail.com>
2015-12-09 22:11:17 +0300

    sofrSerial

--- serialSdGsmRtc/serialSdGsmRtc.ino
+++ serialSdGsmRtc/serialSdGsmRtc.ino
@@ -1,15 +1,18 @@
 #include <SdFat.h>
 #include <SPI.h>
+#include <SoftwareSerial.h>
 
 const byte SD_CS =  4; //SS
 SdFat SD_card;
 File SD_file_log;
 bool SD_isEnable = false;
 
+SoftwareSerial serialCommand(7, 8); // RX, TX
 String command;
 
 void setup() {
   Serial.begin(9600);
+  serialCommand.begin(9600);
 
   pinMode(SD_CS, OUTPUT);
   pinMode(10, OUTPUT); //hardware ATmega SPI SS
@@ -20,11 +23,11 @@ void setup() {
 void loop() { // run over and over
   bool isEndOfCommand = false;
   char getByte;
-  while (Serial.available() & !isEndOfCommand ) {
-    char getByte = Serial.read();
+  while (serialCommand.available() & !isEndOfCommand ) {
+    char getByte = serialCommand.read();
     command += getByte;
     if (getByte == '{') {
-      command = "";
+      command = "{"; //new command start, delete old (?)
     }
     if (getByte == '}') {
       isEndOfCommand = true;
@@ -41,7 +44,7 @@ void processCommand(String command) {
 
 void SD_log(String command) {
   if (SD_isEnable) {
-    SD_file_log = SD_card.open("events.txt", FILE_WRITE); // 8.3 filename.ext rule
+    SD_file_log = SD_card.open("log.txt", FILE_WRITE); // 8.3 filename.ext rule
     if (SD_file_log) {
       SD_file_log.println(command);
       SD_file_log.close();
