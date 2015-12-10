84f56eb488513f48b18b22490f8b2cda537278f4
Boris <mdeed87@gmail.com>
2015-12-10 21:48:19 +0300

    fck

--- sdConfPersistentStorage/sdConfPersistentStorage.ino
+++ sdConfPersistentStorage/sdConfPersistentStorage.ino
@@ -24,7 +24,7 @@
 #include <SPI.h>
 #include <SdFat.h>
 SdFat SD_card;
-SdFile SD_confFileGSM;
+SdFile SD_filePeriodSms;
 bool SD_isEnable = false;
 
 uint32_t RTC_getUnixtime() {
@@ -32,38 +32,80 @@ uint32_t RTC_getUnixtime() {
   return u;
 }
 
-bool conf_isAllowSMS(char* type) {
+bool conf_isAllowSMS(String type) {
   //смс каждого типа ограничены промежутками. Например, вольты -1 раз в сутки.
   //в файле вольты.sms храним время предыдущей отправки
-  char filename = type;
+  char* filename;
+  type.toCharArray(filename, sizeof(type));
+
   uint32_t currUnixtime = RTC_getUnixtime();
+  char* prevUnixtime_chr = {};
+  uint32_t prevUnixtime = 0;
+  bool res_SDisDamaged = false;
+  bool res_isAllowByPeriod = false;
+
+  uint16_t periodByTypeSms = 100;//seconds  TODO
+
+  Serial.print("curr: ");
+  Serial.print(currUnixtime, DEC);
+  Serial.println(" ");
+
   if (!SD_isEnable) {
-    return false;
+    Serial.println("!SD_isEnable");
+    res_SDisDamaged = true;
+  }
+  /*
+    ----------читается
+    да-----------------------нет
+    берем дату из файла------создаем
+    закрыли
+
+    открыли|создали|обрезали до 0 (truncate)
+    пишем текущую дату
+    закрыли
+  */
+
+  if (SD_filePeriodSms.open(filename, O_READ)) { //check period
+    Serial.println("open_read");
+    uint8_t i = 0;
+    while (SD_filePeriodSms.available()) {
+      prevUnixtime_chr[i] = SD_filePeriodSms.read();
+      i++;
+    }
+    prevUnixtime = atol(prevUnixtime_chr);
+    SD_filePeriodSms.close();
+    Serial.print("curr: ");
+    Serial.print(prevUnixtime, DEC);
+    Serial.println(" ");
+    res_isAllowByPeriod = (bool) (currUnixtime - prevUnixtime) > periodByTypeSms;
+  }
+  else {
+    Serial.println("!open_read");
+    res_SDisDamaged = true;
   }
 
-----------читается
-да-----------------------нет
-берем дату из файла------создаем
-закрыли
-
-открыли|создали|обрезали до 0 (truncate)
-пишем текущую дату
-закрыли
-
-  
-  if (!SD_confFileGSM.open(filename, O_READ)) {
-    if (!SD_confFileGSM.open(filename, O_WRITE | O_CREAT | O_TRUNC )) {
-      return false;
-    }
-    else {
-      myFile.println("");
-    }
+  if (SD_filePeriodSms.open(filename, O_WRITE | O_CREAT | O_TRUNC )) {
+    Serial.println("open_w");
+    SD_filePeriodSms.println(currUnixtime);
+    SD_filePeriodSms.close();
+  }
+  else {
+    Serial.println("!open_w");
+    res_SDisDamaged = true;
   }
 
+  if (res_SDisDamaged) {
+    return true; //sd damaged || create new => send SMS and send, balanse will be 0
+  }
+  else {
+    return res_isAllowByPeriod;
+  }
 }
 
 
 void setup() {
+  Serial.begin(9600);
+  delay(1000);
   if (SD_card.begin(10, SPI_HALF_SPEED)) {
     SD_isEnable = true;
   } else {
@@ -72,5 +114,9 @@ void setup() {
 }
 
 void loop() {
+  String type = "wleak.sms";
+  bool isAllow = conf_isAllowSMS(type);
+  Serial.println(" isAllow" + isAllow);
+  delay(2000);
 
 }
