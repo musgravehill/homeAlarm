2af121cf1e8289dd356e5ae20a4d52a90b4ea7b6
Bob_work <musgravehill@gmail.com>
2015-12-10 10:09:00 +0400

    pasre

--- stringParse/stringParse.ino
+++ stringParse/stringParse.ino
@@ -1,12 +1,21 @@
 void setup() {
   Serial.begin(9600);
-  String command = "LOGS=#2;V3.7;T23;H50" ; // + '\0';
-  String command2 = "ALRT=#3;T23" ; // + '\0'; //\0 for strtok, terminated string
+  String command1 = "LOGS;#2;V3.7;T23;H50" + '\0';
+  String command2 = "ALRT;#3;T23" ; // + '\0'; //\0 for strtok, terminated string
 
-  String commandType = command.substring(0,4);
+  parseCommand(command1);
+  parseCommand(command2);
+}
+
+void parseCommand(String command) {
+  Serial.println("\r\n IN: " + command);
+  String commandType = command.substring(0, 4);
+  String commandText = command.substring(5);
   Serial.println(commandType);
+  Serial.println(commandText);
 }
 
 void loop() {
 
 }
+
