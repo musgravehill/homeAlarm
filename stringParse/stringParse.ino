f1059bd0f76a7532d551f733ae87ae563b1de351
Bob_work <musgravehill@gmail.com>
2015-12-10 10:49:58 +0400

    commands

--- stringParse/stringParse.ino
+++ stringParse/stringParse.ino
@@ -3,16 +3,40 @@ void setup() {
   String command1 = "LOGS;#2;V3.7;T23;H50" + '\0';
   String command2 = "ALRT;#3;T23" ; // + '\0'; //\0 for strtok, terminated string
 
-  parseCommand(command1);
-  parseCommand(command2);
+  processCommand(command1);
+  processCommand(command2);
 }
 
-void parseCommand(String command) {
-  Serial.println("\r\n IN: " + command);
+void processCommand(String command) {
+  byte minute = 55;
+  byte hour = 14;
+  byte dayOfMonth = 31;
+  byte month = 12;
+  byte year = 15;
+
+
+  String message = command + ";" + hour + ":" +  minute + " " + dayOfMonth + "-" + month + "-" + year;
   String commandType = command.substring(0, 4);
-  String commandText = command.substring(5);
-  Serial.println(commandType);
-  Serial.println(commandText);
+  logSD(message);
+  if (commandType == "ALRT") { //LOGS, ALRT
+    sendSMS(message, "+79998885533");
+  }
+
+  //Serial.println("\r\n IN: " + command);
+  //Serial.println(commandType);
+}
+
+void sendSMS(String message, String phone) {
+  Serial.rintln("AT+CMGS=\"" + phone + "\"");
+  delay(1000);
+  Serial.print(message);
+  delay(300);
+  Serial.print((char)26);
+  delay(300);
+}
+
+void logSD(String message) {
+  Serial.println("SD: " + message);
 }
 
 void loop() {
