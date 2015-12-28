5d3627f4055118ee9c53062992a183a2fa7e490f
Bob_work <musgravehill@gmail.com>
2015-12-28 10:35:07 +0400

    led

--- DEV/ledDirect/ledDirect.ino
+++ DEV/ledDirect/ledDirect.ino
@@ -10,10 +10,12 @@ byte LED_twoByte1;
 byte LED_twoByte2;
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(LED_latchPin, OUTPUT);
   pinMode(LED_dataPin, OUTPUT);
   pinMode(LED_clockPin, OUTPUT);
-  
+
   LED_twoBytes = 0b0;
   bitWrite(LED_twoBytes, 0, 1);
   bitWrite(LED_twoBytes, 9, 1);
@@ -23,10 +25,29 @@ void setup() {
   LED_twoByte2 = lowByte(LED_twoBytes);
   shiftOut(LED_dataPin, LED_clockPin, MSBFIRST, LED_twoByte1);
   shiftOut(LED_dataPin, LED_clockPin, MSBFIRST, LED_twoByte2);
-  digitalWrite(LED_latchPin, HIGH); //leds ready  
+  digitalWrite(LED_latchPin, HIGH); //leds ready
 
 }
 
 void loop() {
+  int val = analogRead(A5);
 
+  digitalWrite(LED_latchPin, LOW); //leds off
+  LED_twoBytes = 0b0;
+  if (val > 230) {
+    bitWrite(LED_twoBytes, 0, 0);
+    bitWrite(LED_twoBytes, 1, 1);
+  }
+  else {
+    bitWrite(LED_twoBytes, 0, 1);
+    bitWrite(LED_twoBytes, 1, 0);
+  }
+  
+  LED_twoByte1 = highByte(LED_twoBytes);
+  LED_twoByte2 = lowByte(LED_twoBytes);
+  shiftOut(LED_dataPin, LED_clockPin, MSBFIRST, LED_twoByte1);
+  shiftOut(LED_dataPin, LED_clockPin, MSBFIRST, LED_twoByte2);
+  digitalWrite(LED_latchPin, HIGH); //leds ready
+  Serial.println(val, DEC);
+  delay(500);
 }
