6ad25a02362e5d07e17bd2d9034f00c4a2a567ee
Boris <mdeed87@gmail.com>
2015-12-30 23:11:25 +0300

    base rtc

--- base2560/RTC.ino
+++ base2560/RTC.ino
@@ -3,7 +3,8 @@ void RTC_setTime() {
   SYS_DS3231.setMonth(12);
   SYS_DS3231.setDate(30);
   SYS_DS3231.setDoW(2); //Sets the Day of the Week (1-7);
-  SYS_DS3231.setHour(22);
-  SYS_DS3231.setMinute(6);
+  SYS_DS3231.setHour(23);
+  SYS_DS3231.setMinute(11);
   SYS_DS3231.setSecond(0);
+  SYS_DS3231.enableOscillator(true, true, 0);
 }
