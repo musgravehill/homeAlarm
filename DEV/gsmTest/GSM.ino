d8c481af79842db95f48a7b1bc3419a451995509
Bob_work <musgravehill@gmail.com>
2015-12-31 10:12:30 +0400

    dev

---
+++ DEV/gsmTest/GSM.ino
@@ -0,0 +1,24 @@
+void GSM_init() {
+  gsmSerial.println("AT+CLIP=1"); //turn on caller ID notification
+  delay(100);
+  gsmSerial.println("AT+CMGF=1"); //Switching to text mode
+  delay(100);
+  gsmSerial.println("AT+CSCS=\"GSM\""); //english only
+  delay(100);
+}
+
+void GSM_sendSMS(String message, String phone) {
+  gsmSerial.println("AT+CMGS=\"" + phone + "\"");
+  delay(1000);
+  gsmSerial.print(message);
+  delay(300);
+  gsmSerial.print((char)26);
+  delay(300);
+}
+
+void GSM_cleanAllSMS() {
+  gsmSerial.println("AT+CMGD=1,4"); //clean ALL SMS (in, out, read, unread, sent, unsent)
+  delay(50);
+}
+
+
