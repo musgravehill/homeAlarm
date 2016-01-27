81722cd2a7184e98f7450d4788b95e1651859dbc
Boris K.V <musgravehill@gmail.com>
2016-01-27 20:23:38 +0300

    lowpower+wdt

--- DEV/watchDogTest/watchDogTest.ino
+++ DEV/watchDogTest/watchDogTest.ino
@@ -1,25 +1,51 @@
 #include <avr/wdt.h>
+#include "LowPower.h"
 
 void setup() {
   MCUSR = 0;  //VERY VERY IMPORTANT!!!! ELSE WDT DOESNTOT RESET, DOESNOT DISABLED!!!
   wdt_disable();
-  Serial.begin(9600);
-  delay(100);
-  Serial.println("Setup WDT 8s init");
-  wdt_enable (WDTO_8S);
-  delay(100);
-  wdt_reset();
   pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+
+  digitalWrite(2, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  delay(100);
 }
 
-int timer = 0;
-
 void loop() {
+  wdt_enable (WDTO_8S);
+  digitalWrite(3, HIGH);
   delay(500);
-  digitalWrite(2, LOW);  
+  digitalWrite(3, LOW);
   delay(500);
-  digitalWrite(2, HIGH);
-  timer++;
-  Serial.flush();
-  Serial.println(timer, DEC);  
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(3, LOW);
+  delay(500);
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(3, LOW);
+  delay(7000);
+
+  wdt_reset();
+  wdt_disable();
+
+  uint8_t countSleep = 0;
+  while (countSleep < 10) {
+    LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+    countSleep++;
+  }
 }
